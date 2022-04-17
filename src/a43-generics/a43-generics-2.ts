66e2971fde799f6dafda69314731816dce167979
Ygor Oliveira <ygoroliveira55@gmail.com>
2022-04-17 11:44:02 -0300

    generics

---
+++ src/a43-generics/a43-generics-2.ts
@@ -0,0 +1,17 @@
+type MeuTipo = number;
+const arrayNumeros: Array<number> = [1, 2, 3, 4, 5, 6];
+
+async function promiseAsync() {
+  return 1;
+}
+
+export function minhaPromise(): Promise<MeuTipo | number> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 1000);
+  });
+}
+
+promiseAsync().then((resultado) => console.log(resultado + 1));
+minhaPromise().then((resultado) => console.log(resultado + 1));
